4ded18ae296f7edd0a0bdf314275c7bf89ea62a4
Animediscussion <prasan1010jeet@gmail.com>
2026-08-24 20:08:09 +0530

    update

---
+++ server/models/Patient.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const patientSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    age: {
+      type: Number,
+      required: true,
+    },
+
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      required: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+    },
+
+    address: {
+      type: String,
+    },
+
+    bloodGroup: {
+      type: String,
+    },
+
+    medicalHistory: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Patient", patientSchema);
