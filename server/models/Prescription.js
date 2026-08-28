2a7b65155756488df98efec5f870c7dbf9075936
Animediscussion <prasan1010jeet@gmail.com>
2026-08-28 09:09:54 +0530

    update

---
+++ server/models/Prescription.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const prescriptionSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true,
+    },
+
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+
+    medicines: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+
+        dosage: {
+          type: String,
+        },
+
+        frequency: {
+          type: String,
+        },
+
+        duration: {
+          type: String,
+        },
+      },
+    ],
+
+    notes: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Prescription", prescriptionSchema);
